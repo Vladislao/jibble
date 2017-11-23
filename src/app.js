4dea54ea8acf127e6fcc67969ba695312ecec95c
vladislao <netcemetery@yandex.ru>
2017-11-23 06:17:34 +0400

    add example

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 const { authorize } = require('./auth');
 const cache = require('./cache');
+const throttle = require('./throttle');
 
 /*
  * Server configuration
@@ -16,12 +17,20 @@ const app = express();
 //   (key, body) => { store[key] = body; },
 // ));
 
+// Throttle usage example
+// let count = 5;
+// setInterval(() => { count += 1; }, 1000);
+// app.use(throttle(() => {
+//   count -= 1;
+//   return count > 0;
+// }));
+
 app
   .use(authorize)
+  .use(throttle())
   .use(cache())
   .use('/api', routes)
   .use((err, req, res, next) => {
-    // log error
     console.error(err);
 
     res.status(500).end();
